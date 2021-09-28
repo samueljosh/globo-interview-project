e60375585e4851f251e37bc7ea0576769aa2cf5b
sa.oliveira <sa.oliveira@reply.com>
2021-09-28 18:57:16 -0300

    Ultimo commit

--- globo-interview-project-service/src/configurations/db.js
+++ globo-interview-project-service/src/configurations/db.js
@@ -1,11 +1,12 @@
-const Sequelize = require('sequelize')
-
-const globoDbConfig = new Sequelize({
-    dialect: 'sqlite',
-    storage: './globo_interview_project_db.sqlite',
-
-});
 
 
-module.exports = globoDbConfig;
+
+import * as tedious from 'tedious';
+
+const Sequelize = require('sequelize');
+const db = new Sequelize('globoinverviewdatabase', 'login123456', 'senha123456',{dialect:"mssql", dialectModule : tedious, host: "globoinverviewdatabase.database.windows.net"});
+
+export {
+    db
+}
 
